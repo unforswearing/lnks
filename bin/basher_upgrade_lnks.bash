4f6b2c3be84d56bf739912216ef43197be71c388
Alvin <al.charity@gmail.com>
2025-03-20 23:40:36 -0400

    reorganize folders

--- bin/basher_upgrade_lnks.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ -z "$1" ]] && {
-  echo "No commit message passed to update function."
-} || {
-  git add . && \
-    git commit -m "$1" && \
-    git push && basher upgrade unforswearing/lnks
-}
--- bin/debug.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-debug() {
-	red="\033[31m"; reset="\033[39m";
-	echo -en ""$red"[line:${LINENO-:BASH_LINENO}] $1"$reset"\n"
-}
