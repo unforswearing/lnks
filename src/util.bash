12735343800cdc565df31de9437261517cfd18b8
Alvin <al.charity@gmail.com>
2025-03-21 14:38:01 -0400

    remove util.prog

---
+++ src/util.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#!/bin/zsh
+# ref: zsh 5.9 (x86_64-apple-darwin24.0)
+function _util.color() {
+  local red="\033[31m"
+  local green="\033[32m"
+  local blue="\033[34m"
+  local reset="\033[39m"
+  local opt="$1"
+  shift
+  case "$opt" in
+    red) print "${red}$*${reset}" ;;
+    green) print "${green}$*${reset}" ;;
+    blue) print "${blue}$*${reset}" ;;
+  esac
+}
+function _util.require() {
+  test "$(command -v "$1")"
+}
